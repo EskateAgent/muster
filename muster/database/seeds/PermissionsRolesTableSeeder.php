569069513278fcf5a6ca336e550088d2d448fdcc
Adrian Humphrey <github@ember.co.nz>
2015-03-31 22:39:27 +0100

    added home permission

--- muster/database/seeds/PermissionsRolesTableSeeder.php
+++ muster/database/seeds/PermissionsRolesTableSeeder.php
@@ -11,41 +11,45 @@ class PermissionsRolesTableSeeder extends Seeder {
 
     $permissions = array(
       // root user
-      ['role_id' => 1, 'permission_id' => 1 ],  // user-show
-      ['role_id' => 1, 'permission_id' => 2 ],  // user-create
-      ['role_id' => 1, 'permission_id' => 3 ],  // user-edit
-      ['role_id' => 1, 'permission_id' => 4 ],  // user-destroy
-      ['role_id' => 1, 'permission_id' => 5 ],  // league-show
-      ['role_id' => 1, 'permission_id' => 6 ],  // league-create
-      ['role_id' => 1, 'permission_id' => 7 ],  // league-edit
-      ['role_id' => 1, 'permission_id' => 8 ],  // league-destroy
-      ['role_id' => 1, 'permission_id' => 9 ],  // charter-show
-      ['role_id' => 1, 'permission_id' => 10 ], // charter-create
-      ['role_id' => 1, 'permission_id' => 11 ], // charter-edit
-      ['role_id' => 1, 'permission_id' => 12 ], // charter-destroy
-      ['role_id' => 1, 'permission_id' => 13 ], // charter-request_approval
-      ['role_id' => 1, 'permission_id' => 14 ], // charter-approve
-      ['role_id' => 1, 'permission_id' => 15 ], // charter-reject
+      ['role_id' => 1, 'permission_id' => 1 ],  // home
+      ['role_id' => 1, 'permission_id' => 2 ],  // user-show
+      ['role_id' => 1, 'permission_id' => 3 ],  // user-create
+      ['role_id' => 1, 'permission_id' => 4 ],  // user-edit
+      ['role_id' => 1, 'permission_id' => 5 ],  // user-destroy
+      ['role_id' => 1, 'permission_id' => 6 ],  // league-show
+      ['role_id' => 1, 'permission_id' => 7 ],  // league-create
+      ['role_id' => 1, 'permission_id' => 8 ],  // league-edit
+      ['role_id' => 1, 'permission_id' => 9 ],  // league-destroy
+      ['role_id' => 1, 'permission_id' => 10 ], // charter-show
+      ['role_id' => 1, 'permission_id' => 11 ], // charter-create
+      ['role_id' => 1, 'permission_id' => 12 ], // charter-edit
+      ['role_id' => 1, 'permission_id' => 13 ], // charter-destroy
+      ['role_id' => 1, 'permission_id' => 14 ], // charter-request_approval
+      ['role_id' => 1, 'permission_id' => 15 ], // charter-approve
+      ['role_id' => 1, 'permission_id' => 16 ], // charter-reject
 
       // staff
-      ['role_id' => 2, 'permission_id' => 1 ],  // user-show
-      ['role_id' => 2, 'permission_id' => 3 ],  // user-edit
-      ['role_id' => 2, 'permission_id' => 5 ],  // league-show
-      ['role_id' => 2, 'permission_id' => 6 ],  // league-create
-      ['role_id' => 2, 'permission_id' => 7 ],  // league-edit
-      ['role_id' => 2, 'permission_id' => 9 ],  // charter-show
-      ['role_id' => 2, 'permission_id' => 14 ], // charter-approve
-      ['role_id' => 2, 'permission_id' => 15 ], // charter-reject
+      ['role_id' => 2, 'permission_id' => 1 ],  // home
+      ['role_id' => 2, 'permission_id' => 2 ],  // user-show
+      ['role_id' => 2, 'permission_id' => 3 ],  // user-create
+      ['role_id' => 2, 'permission_id' => 4 ],  // user-edit
+      ['role_id' => 2, 'permission_id' => 6 ],  // league-show
+      ['role_id' => 2, 'permission_id' => 7 ],  // league-create
+      ['role_id' => 2, 'permission_id' => 8 ],  // league-edit
+      ['role_id' => 2, 'permission_id' => 10 ], // charter-show
+      ['role_id' => 2, 'permission_id' => 15 ], // charter-approve
+      ['role_id' => 2, 'permission_id' => 16 ], // charter-reject
 
       // league
-      ['role_id' => 3, 'permission_id' => 1 ],  // user-show
-      ['role_id' => 3, 'permission_id' => 3 ],  // user-edit
-      ['role_id' => 3, 'permission_id' => 5 ],  // league-show
-      ['role_id' => 3, 'permission_id' => 7 ],  // league-edit
-      ['role_id' => 3, 'permission_id' => 9 ],  // charter-show
-      ['role_id' => 3, 'permission_id' => 10 ], // charter-create
-      ['role_id' => 3, 'permission_id' => 11 ], // charter-edit
-      ['role_id' => 3, 'permission_id' => 13 ], // charter-request_approval
+      ['role_id' => 3, 'permission_id' => 1 ],  // home
+      ['role_id' => 3, 'permission_id' => 2 ],  // user-show
+      ['role_id' => 3, 'permission_id' => 4 ],  // user-edit
+      ['role_id' => 3, 'permission_id' => 6 ],  // league-show
+      ['role_id' => 3, 'permission_id' => 8 ],  // league-edit
+      ['role_id' => 3, 'permission_id' => 10 ], // charter-show
+      ['role_id' => 3, 'permission_id' => 11 ], // charter-create
+      ['role_id' => 3, 'permission_id' => 12 ], // charter-edit
+      ['role_id' => 3, 'permission_id' => 14 ], // charter-request_approval
     );
 
     // Uncomment the below to run the seeder
