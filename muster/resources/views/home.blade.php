ec504f3c3efa1ba33610abc6caa4f062c5d60e7b
Adrian Humphrey <github@ember.co.nz>
2015-04-22 23:28:49 +0100

    started overhauling laravel default styling with bootstrap template styling

--- muster/resources/views/home.blade.php
+++ muster/resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('app')
 
 @section('content')
-  <h2>All Leagues</h2>
+  <div class="page-header">
+    <h1>All Leagues</h1>
+  </div>
 
   @if( !$leagues->count() )
-    No leagues
+    <p>No leagues</p>
   @else
-    <table>
+    <table class="table table-striped">
       <thead>
         <tr>
           <th rowspan="2">League</th>
-          <th colspan="{{ count( $charter_types ) }}">Current Charters</th>
-          <th rowspan="2">User</th>
+          <th rowspan="2" style="border-right: 2px solid #ddd">User</th>
+          <th colspan="{{ count( $charter_types ) }}" style="text-align: center; border-bottom: 1px solid #ddd;">Current Charters</th>
         </tr>
         <tr>
           @foreach( $charter_types as $type )
@@ -20,27 +22,28 @@
         </tr>
       </thead>
       <tbody>
-      @foreach( $leagues as $league )
-        <tr>
-          <td><a href="{{ route('leagues.show', [ $league->slug ] ) }}">{{ $league->name }}</a></td>
-          @foreach( $charter_types as $type )
-          <td>
-            @if( $charter = $league->currentCharter( $type->id ) )
-              <a href="{{ route('leagues.charters.show', [ $league->slug, $charter->slug ] ) }}">{{ $charter->name }}</a>
-            @else
-              -
-            @endif
-          </td>
-          @endforeach
-          <td>
-            @if( $user = $league->user )
-              <a href="{{ route('users.show', [ $league->user->id ] ) }}">{{ $user->name }}</a>
-            @else
-              -
-            @endif
-          </td>
-        </tr>
-      @endforeach
+        @foreach( $leagues as $league )
+          <tr>
+            <td><a href="{{ route('leagues.show', [ $league->slug ] ) }}">{{ $league->name }}</a></td>
+            <td style="border-right: 2px solid #ddd;">
+              @if( $user = $league->user )
+                <a href="{{ route('users.show', [ $league->user->id ] ) }}">{{ $user->name }}</a>
+              @else
+                -
+              @endif
+            </td>
+            @foreach( $charter_types as $type )
+            <td>
+              @if( $charter = $league->currentCharter( $type->id ) )
+                <a href="{{ route('leagues.charters.show', [ $league->slug, $charter->slug ] ) }}">{{ $charter->name }}</a>
+              @else
+                -
+              @endif
+            </td>
+            @endforeach
+          </tr>
+        @endforeach
+      </tbody>
     </table>
   @endif
 @endsection
