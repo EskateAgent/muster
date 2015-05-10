2032c54f68fc3901f6e3c8ccf97affb4d00f88eb
Adrian Humphrey <github@ember.co.nz>
2015-05-10 13:54:48 +0100

    reworked base email template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Muster</title>
 
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
--- resources/views/emails/base.blade.php
+++ resources/views/emails/base.blade.php
@@ -1,5 +1,54 @@
-<p>Hi {{ $name }},</p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        background: #ddd;
+        font-family: Helvetica, Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+        .container {
+          background: #fff;
+          overflow: hidden;
+          padding: 15px 30px;
+          border: 1px solid #aaa;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+          margin: 30px;
+        }
+          a {
+            color: #337ab7;
+            text-decoration: none;
+          }
+          a:active,
+          a:focus,
+          a:hover {
+            color: #23527c;
+            text-decoration: underline;
+          }
+          .footer {
+            margin: 30px 0 0;
+            font-size: 0.8em;
+            color: #888;
+          }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <p>Hi {{ $name }},</p>
 
-@yield('content')
+      @yield('content')
 
-<p>Kind regards,<br /><br />The UKRDA Email Robot</p>
+      <br />
+      <p>Kind regards,<br /><br />The UKRDA Email Robot</p>
+      <div class="footer">
+        <p>This message was sent to you automatically on behalf of the <a href="http://ukrda.org.uk/">United Kingdom Roller Derby Association</a> by <a href="http://muster.ukrda.org.uk/">Muster</a>.</p>
+      </div>
+    </div>
+  </body>
+</html>
