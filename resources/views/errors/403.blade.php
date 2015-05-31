6af44939d31b0d66e33736b1c3280f0f56a78b24
Adrian Humphrey <github@ember.co.nz>
2015-05-31 14:33:34 +0100

    added a nearly-identical-to-the-404 error page for 403s

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,34 @@
+<?php
+
+  $pictures = array(
+                    array('name' => 'duckling.jpg', 'description' => 'a quizzical duckling'),
+                    array('name' => 'elephant.jpg', 'description' => 'a very happy elephant'),
+                    array('name' => 'flamingo.jpg', 'description' => 'an awkward, baby flamingo'),
+                    array('name' => 'glass.jpg', 'description' => 'a snifter of kitty'),
+                    array('name' => 'kitten.jpg', 'description' => 'a teeny tiny kitten'),
+                    array('name' => 'owls.jpg', 'description' => 'some kissing owls'),
+                    array('name' => 'panda.jpg', 'description' => 'a panda'),
+                    array('name' => 'puppies.jpg', 'description' => 'a stack of cute puppies'),
+                    array('name' => 'puppy.jpg', 'description' => 'a puppy munching a shoe'),
+                    array('name' => 'rabbits.jpg', 'description' => 'kissing rabbits'),
+                    array('name' => 'red-pandas.jpg', 'description' => 'high-fiving red pandas'),
+                    array('name' => 'tiger.jpg', 'description' => 'an unimpressed tiger'),
+                    array('name' => 'zebra.jpg', 'description' => 'a frolicking zebra foal'),
+  );
+
+  $selected = array_rand( $pictures );
+
+?>
+
+@extends('app')
+
+@section('content')
+  <div class="page-header">
+    <h1>Oh dear!</h1>
+  </div>
+  <p>A thing seems to have not gone to plan and broken a wee bit.</p>
+  <p>Sorry about that.</p>
+  <p>&nbsp;</p>
+  <p>Perhaps this picture of {{ $pictures[ $selected ]['description'] }} will make you feel better?</p>
+  <p><img src="/images/404/{{ $pictures[ $selected ]['name'] }}" alt="{{ $pictures[ $selected ]['description'] }}" /></p>
+@endsection
