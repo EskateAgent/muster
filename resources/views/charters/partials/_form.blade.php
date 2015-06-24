873d1b4943ab2a99c457f29e3731ee47a65cbd72
Adrian Humphrey <github@ember.co.nz>
2015-06-24 22:15:50 +0100

    added example csv file to charter form

--- resources/views/charters/partials/_form.blade.php
+++ resources/views/charters/partials/_form.blade.php
@@ -13,3 +13,12 @@
 <div class="form-group">
   {!! Form::submit('Save', ['class' => 'btn btn-primary'] ) !!}
 </div>
+
+<br /><br />
+<aside>
+  <span class="icon info"></span>
+  <p>If you're not sure what format your <abbr title="Comma-separated value">CSV</abbr> file needs to be in, have a look at this <a href="/assets/example.csv">example file</a> as a starting point.</p>
+  <p>As long as your CSV matches the same columns as the example file, we'll do our best to convert that into a meaningful charter.</p>
+  <p>&nbsp;</p>
+  <p>Incidentally, if your league is WFTDA-affiliated, then you can also use the CSV files which Mothership exports without any modification!</p>
+</aside>
